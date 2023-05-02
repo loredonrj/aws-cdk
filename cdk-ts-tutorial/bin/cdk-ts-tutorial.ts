1af5a482c212db2f953592f74016f714dfb2a43f
loredonrj <loredonrj@users.noreply.github.com>
2023-05-02 04:22:16 +0000

    changed stack to dev & gen CloudFormation template

--- cdk-ts-tutorial/bin/cdk-ts-tutorial.ts
+++ cdk-ts-tutorial/bin/cdk-ts-tutorial.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkTsTutorialStack } from '../lib/cdk-ts-tutorial-stack';
 
 const app = new cdk.App();
-new CdkTsTutorialStack(app, 'CdkTsTutorialStack', {
+new CdkTsTutorialStack(app, 'CdkTsTutorialStack-dev', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
